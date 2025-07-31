736e5ffb0248093df67347e194db903497890b25
Code_Mr_Fan <1738874436@qq.com>
2025-07-31 11:06:40 +0800

    my_first_Demo hello word

--- Wpf_Study/MainWindow.xaml.cs
+++ Wpf_Study/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Wpf_Study
         {
             InitializeComponent();
         }
+
+        private void Clik_me_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxShowHello.Text= "hello world !";
+        }
     }
 }
