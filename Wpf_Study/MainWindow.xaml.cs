10a1c7791c3708658c48a76736eedb82ceb1b7c3
Code_Mr_Fan <1738874436@qq.com>
2025-08-31 22:51:27 +0800

    mian

--- Wpf_Study/MainWindow.xaml.cs
+++ Wpf_Study/MainWindow.xaml.cs
@@ -72,6 +72,7 @@ namespace Wpf_Study
         // 就地放一个 RelayCommand
         public class RelayCommand : ICommand
         {
+
             private readonly Action<object> _execute;
             public RelayCommand(Action<object> execute) => _execute = execute;
             public bool CanExecute(object parameter) => true;
